73adb4db013f597b814849d7618b23dcdbb702f2
Ulrich <songs.of.letting.go@gmail.com>
2018-01-29 23:58:22 +0100

    Refined Graph

--- UseComplexMapping/src/de/ulrich_boeing/sketches/RandomTransition.java
+++ UseComplexMapping/src/de/ulrich_boeing/sketches/RandomTransition.java
@@ -24,7 +24,7 @@ public class RandomTransition extends PApplet {
 	public void setup() {
 		map = getRandomMap();
 		
-		cycleMap = Map.create("narrow 0.0, 0.6 > exp 4", Precision.VeryHigh).setRange(0, cycleLength, 0, 1);
+		cycleMap = Map.create("narrow 0.0, 0.5 > exp 3", Precision.VeryHigh).setRange(0, cycleLength, 0, 1);
 	}
 
 	@Override
@@ -33,17 +33,19 @@ public class RandomTransition extends PApplet {
 		
 		float normCyclePos = getNormCyclePos();
 		for (int i = 0; i < height; i++) {
-			stroke(32, 0, map.get(i, normCyclePos) / 7);
+			stroke(map.get(i, normCyclePos) / 18 , 0, map.get(i, normCyclePos) / 7);
 			line(0, i, width, i);
 		}
 
+		stroke(255, 0, 255, 18);
+		fillGraph(normCyclePos);
 		noFill();
-		stroke(255);
-		strokeWeight(1.5f);
+		stroke(255, 100);
+		strokeWeight(1f);
 		drawGraph(normCyclePos);
 		// Show DefString
 		textSize(16);
-		fill(255, 255, 255, normCyclePos * 155 + 100);
+		fill(255, 255, 255, normCyclePos * 200 + 55);
 		textAlign(CENTER);
 		text(end, width / 2, height -20);
 		
@@ -55,6 +57,12 @@ public class RandomTransition extends PApplet {
 			vertex(x, map.get(x, normCyclePos));
 		}
 		endShape();
+	}
+
+	private void fillGraph(float normCyclePos) {
+		for (float x = 0; x < width; x++) {
+			line(x, map.get(x, normCyclePos), x, 0);
+		}
 	}
 	
 	private float getNormCyclePos() {
@@ -76,7 +84,7 @@ public class RandomTransition extends PApplet {
 		start = end;
 		end = mapGenerator.getRandomDefString();
 		Map randomMap = Map.create(start, Precision.High)
-				.setRange(1, width - 1, height - 50, 15).setTargetMap(end, Precision.Highest);
+				.setRange(1, width - 1, height - 50, 15).setTargetMap(end, Precision.High);
 		
 		System.out.println(randomMap.toString());
 
